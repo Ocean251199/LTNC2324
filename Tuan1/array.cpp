3caadb1370352551e2f19fb5378697d8a0eaab0e
Ocean251199 <104547483+Ocean251199@users.noreply.github.com>
2024-04-25 13:51:58 +0700

    Create array.cpp

---
+++ Tuan1/array.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int N;
+    cin >> N;
+
+    int a[N]; // Declaring an array of size N
+
+    // Input integers into the array
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+
+    // Print integers in reverse order
+    for (int j = N - 1; j >= 0; j--) {
+        cout << a[j] << " ";
+    }
+
+    return 0;
+}
